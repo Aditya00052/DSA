4638c4fbee9cae08aef317a36a0e4e59c93e09e6
Aditya Azad Singh <111888942+Aditya00052@users.noreply.github.com>
2025-11-18 07:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.37%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,9 +1,16 @@
+# Time complexity = O(n); as we are using hash tables for faster lookup
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
+        hashmap = {}
+
         for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            hashmap[nums[i]] = i
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
         
         return [0, 0]
